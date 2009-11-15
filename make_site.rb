4f79ea29e9b8c654a4ff8950f5e3cc15f68c79aa
Akeem Adeniji <akeem.adeniji@gmail.com>
2009-11-15 10:42:13 -0500

    allowed script to now specify output directory via command line arguement

--- make_site.rb
+++ make_site.rb
@@ -1,6 +1,18 @@
 directories = ['images','scripts','style']
 css_file = 'style.css'
 
+if ARGV.size == 0
+  output_directory = "."
+elsif ARGV.size == 1
+  output_directory = ARGV[0]
+end  
+
+if File.exist?(output_directory) == false
+   Dir.mkdir(output_directory)
+end   
+
+Dir.chdir(output_directory)
+
 for directory in directories
   Dir.mkdir(directory)
 end
